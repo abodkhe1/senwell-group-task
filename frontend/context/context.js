5585150414a56559dc32f26b2726daf8d8377cc3
abodkhe1 <122731356+abodkhe1@users.noreply.github.com>
2024-08-16 23:56:07 +0530

    Update context.js

--- frontend/context/context.js
+++ frontend/context/context.js
@@ -1,17 +1,1 @@
-import { createContext } from "react";
 
-const TodoContext = createContext({
-    todos: [
-        { userId: 1, id: 1, title: 'delectus aut autem', completed: false }
-    ],
-    getData: () => { },
-})
-
-
-const useTodo = () => {
-    return useContext(TodoContext)
-}
-
-const TodoProvider = TodoContext.Provider;
-
-export default { TodoContext, TodoProvider, useTodo }
